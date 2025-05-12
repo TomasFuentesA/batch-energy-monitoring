3f48782bd7bf2a9eb8262fb1d358b496d6f7119e
TomasFuentesA <fuentesarayat@gmail.com>
2025-05-12 12:11:46 -0400

    Clean and Raw data

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,13 @@
-CREATE TABLE IF NOT EXISTS energy_data (
+CREATE TABLE IF NOT EXISTS energy_data_cleaned (
+    timestamp TIMESTAMP NOT NULL,
+    house_id VARCHAR(50) NOT NULL,
+    consumption_kWh FLOAT,
+    temperature FLOAT,
+    voltage FLOAT,
+    PRIMARY KEY (timestamp, house_id)
+);
+
+CREATE TABLE IF NOT EXISTS energy_data_raw (
     timestamp TIMESTAMP NOT NULL,
     house_id VARCHAR(50) NOT NULL,
     consumption_kWh FLOAT,
