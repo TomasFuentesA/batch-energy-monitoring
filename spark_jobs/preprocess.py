0a6522fba4b2ab22f81d3b3e92c722d33f108b5f
TomasFuentesA <fuentesarayat@gmail.com>
2025-05-12 17:48:25 -0400

    Fix clean data

--- spark_jobs/preprocess.py
+++ spark_jobs/preprocess.py
@@ -27,7 +27,9 @@ try:
 
 
     df_clean = consumption_customer.dropna()
-    df_clean = df_clean.filter((df_clean['consumption_kWh'] > 0) & (df_clean['voltage'] != 9999) & (df_clean['voltage'] != -9999))
+    df_clean = df_clean.filter((df_clean['consumption_kWh'] > 0) & (df_clean['consumption_kWh'] != 9999))
+    df_clean = df_clean.filter((df_clean['voltage'] != 9999) & (df_clean['voltage'] != -9999))
+    df_clean = df_clean.filter((df_clean['temperature'] != 9999) & (df_clean['temperature'] != -9999))
     #df_after_clean = df_clean.writeStream.format("console").outputMode("append").option("truncate", False).start()
     df_after_clean = df_clean.writeStream.format("csv").outputMode("append").option("path", f"{current_path}/data/clean").option("checkpointLocation", f"{current_path}/data/clean/checkpoint").start().awaitTermination()
     
